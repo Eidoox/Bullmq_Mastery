1a75d625a5d7fa9ff53f21b84617e7d5701c9643
Eidoox <ahmedeidabdullah99@gmail.com>
2025-11-22 22:02:09 +0200

    add book model

---
+++ prisma/migrations/20251122200022_add_user_and_book_models/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
